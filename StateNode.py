9e97b554836dab91619817d90c16d1097cfc6ba5
calmzzc <475865994@qq.com>
2022-08-16 17:27:54 +0800

    0816

--- StateNode.py
+++ StateNode.py
@@ -7,6 +7,7 @@ class StateNode:
         self.train_model = train_model  # 列车模型
         self.agent = agent  # 神经网络
         self.ou_noise = ou_noise  # OU噪声
+
         self.state = state  # 当前状态 ,0是时间，1是速度
         self.acc = 0  # 当前合加速度
         self.tm_acc = 0  # 电机牵引加速度
@@ -14,21 +15,32 @@ class StateNode:
         self.g_acc = 0  # 坡度加速度
         self.c_acc = 0  # 曲率加速度
         self.action = np.array(0).reshape(1)  # 当前牵引制动力请求百分比
+
         self.step = step  # 当前阶段
         self.episode = episode  # 当前幕数
+        self.max_step = self.line.length / self.line.delta_distance - 1
         self.current_reward = 0  # 当前奖励
         self.current_q = 0  # 当前Q值
+
         self.last_node_state = 0  # 前一个节点的状态
         self.last_node_action = 0  # 前一个节点的动作
         self.last_node_acc = 0  # 前一个节点的加速度
+
         self.next_state = np.zeros(2)  # 状态转移后的状态
         self.t_power = 0.0  # 状态转移过程的牵引能耗
         self.re_power = 0  # 状态转移过程产生的再生制动能量
+
         self.train_flag = train_flag  # 训练测试标志位
         self.comfort_punish = 0  # 舒适度惩罚标志位
         self.speed_punish = 0  # 超速惩罚标志位
+
         self.current_limit_speed = 0  # 当前限速
         self.ave_v = 0  # 本阶段平均速度
+        self.p_indicator = -10  # 超速惩罚
+
+    # # 获取最大step
+    # def get_max_step(self):
+    #     self.max_step = self.line.length / self.line.delta_distance
 
     def get_last_node(self, node_list):  # 获取上一个节点的状态、动作加速度
         if len(node_list) != 1:
@@ -73,7 +85,7 @@ class StateNode:
             if key_list[j] <= self.step * self.line.delta_distance < key_list[j + 1]:
                 key = key_list[j]
         gradient = self.line.gradient[key]
-        self.g_acc = 9.8 * gradient / 1000  # g_ acc = 9.8 * g /1000
+        self.g_acc = -9.8 * gradient / 1000  # g_ acc = 9.8 * g /1000
         del key_list
 
     def get_curve_acc(self):  # 获取当前位置曲率加速度
@@ -137,7 +149,7 @@ class StateNode:
             self.get_t_power()
 
     # 下面是超速检查过程
-    def get_limit_speed(self):
+    def speed_check(self):
         key_list = []
         key = 0
         for i in self.line.speed_limit.keys():
@@ -159,6 +171,105 @@ class StateNode:
             self.comfort_punish = 1  # 不舒适
         else:
             self.comfort_punish = 0
+
+    # 下面是状态转移函数
+    def get_next_state(self):
+        time = np.array(self.state[0]).reshape(1)
+        temp_velocity = np.array(self.state[1]).reshape(1)
+        temp_square_velocity = temp_velocity * temp_velocity + 2 * self.acc * self.line.delta_distance
+        if temp_square_velocity <= 1:
+            temp_square_velocity = np.array(1).reshape(1)
+        velocity = np.sqrt(temp_square_velocity)
+        if velocity <= 0:
+            velocity = np.array(1).reshape(1)
+        temp_time = self.line.delta_distance / (velocity / 2 + temp_velocity / 2)
+        time = time + temp_time
+        self.next_state[0] = time  # 状态转移后的时间
+        self.next_state[1] = velocity  # 状态转移后的速度
+
+    # 下面是完整的一般状态转移过程
+    def state_transition(self):
+        self.get_action()
+        self.reshape_action()
+        self.get_acc()
+        self.get_next_state()
+        self.get_power()
+
+    # 加入shield后的安全状态转移过程
+    def safe_state_transition(self):
+        self.get_action()
+        self.reshape_action()
+        self.get_acc()
+        self.get_safe_action()
+        self.get_acc()
+        self.get_next_state()
+        self.get_power()
+
+    # 获取安全动作的函数
+    def get_safe_action(self):
+        xunhuan_count = 0
+        chaosu_flag = 0
+        temp_velocity = self.state[1].copy()
+        while chaosu_flag != 1:
+            xunhuan_count += 1
+            if xunhuan_count > 15:
+                temp_acc = self.acc - self.g_acc - self.c_acc
+                if temp_acc >= 0:
+                    self.action = temp_acc * self.train_model.weight / self.train_model.max_traction_force
+                else:
+                    self.action = temp_acc * self.train_model.weight / self.train_model.max_brake_force
+                break
+            temp_square_velocity = temp_velocity * temp_velocity + 2 * self.acc * self.line.delta_distance
+            if temp_square_velocity <= 1:
+                temp_square_velocity = np.array(1).reshape(1)
+            velocity = np.sqrt(temp_square_velocity)
+            if velocity <= 0:
+                velocity = np.array(1).reshape(1)
+            self.state[1] = velocity
+            self.speed_check()
+            if self.speed_punish:
+                self.acc = self.acc - 0.15 * (velocity / self.current_limit_speed)
+            else:
+                chaosu_flag = 1
+                temp_acc = self.acc - self.g_acc - self.c_acc
+                if temp_acc >= 0:
+                    self.action = temp_acc * self.train_model.weight / self.train_model.max_traction_force
+                else:
+                    self.action = temp_acc * self.train_model.weight / self.train_model.max_brake_force
+        self.state[1] = temp_velocity
+
+    # 下面是奖励的计算
+    def get_reward(self, unsafe_counts, total_power):
+        self.speed_check()
+        self.comfort_check()
+        if self.step == self.max_step:
+            done = 1
+            if abs(self.next_state[0] - self.line.scheduled_time) <= 5 and abs(self.next_state[1]) <= 1:
+                e_reward = 100  # 准时且停下施加额外奖励
+            else:
+                e_reward = 0
+            if self.line.scheduled_time - self.next_state[0] < -10:
+                t_punish = -10  # 晚点超过10s的惩罚
+            elif self.line.scheduled_time - self.next_state[0] > 0:
+                t_punish = -self.line.scheduled_time + self.next_state[0]  # 早到的惩罚
+            else:
+                t_punish = -self.next_state[0] + self.line.scheduled_time  # 晚点10s之内的惩罚
+            if self.speed_punish:
+                unsafe_counts += 1
+                self.current_reward = -0.001 * total_power - 25 * self.next_state[1] + 1 * t_punish + e_reward - 10 * self.comfort_punish
+            else:
+                unsafe_counts += 0
+                self.current_reward = -0.001 * total_power - 25 * self.next_state[1] + 1 * t_punish + e_reward - 10 * self.comfort_punish
+        else:
+            done = 0
+            temp_time = self.line.delta_distance / (self.state[1] / 2 + self.next_state[1] / 2)
+            if self.speed_punish:
+                unsafe_counts += 1
+                self.current_reward = -1.5 * self.t_power - 1.5 * self.re_power - 3.4 * abs(1.3 * temp_time - (self.line.scheduled_time / (self.max_step + 1))) + self.p_indicator - 10 * self.comfort_punish
+            else:
+                unsafe_counts += 0
+                self.current_reward = -1.5 * self.t_power - 1.5 * self.re_power - 3.4 * abs(1.3 * temp_time - (self.line.scheduled_time / (self.max_step + 1))) - 10 * self.comfort_punish
+        return done, unsafe_counts
 
     def get_current_q(self):  # 获取当前Q值
         self.current_q = self.agent.target_critic(self.state, self.action.detach())
---
+++ plot.py
@@ -0,0 +1,226 @@
+#!/usr/bin/env python
+# coding=utf-8
+'''
+Author: John
+Email: johnjim0816@gmail.com
+Date: 2020-10-07 20:57:11
+LastEditor: John
+LastEditTime: 2021-09-23 12:23:01
+Discription:
+Environment:
+'''
+import matplotlib.pyplot as plt
+import seaborn as sns
+from matplotlib.font_manager import FontProperties
+import numpy as np
+
+
+def chinese_font():
+    return FontProperties(fname=r"c:\windows\fonts\simsun.ttc", size=15)  # 系统字体路径，此处是mac的
+
+
+def plot_rewards(rewards, ma_rewards, tag="train", env='CartPole-v0', algo="DQN", save=True, path='./'):
+    sns.set()
+    plt.title("average learning curve of {} for {}".format(algo, env))
+    plt.xlabel('epsiodes')
+    plt.plot(rewards, label='rewards')
+    plt.plot(ma_rewards, label='ma rewards')
+    plt.legend()
+    if save:
+        plt.savefig(path + "{}_rewards_curve".format(tag))
+    plt.show()
+
+
+def plot_rewards_cn(rewards, ma_rewards, tag="train", env='Optimal Control', algo="DDPG", save=True, path='./'):
+    ''' 中文画图
+    '''
+    sns.set()
+    plt.figure()
+    plt.title(u"{}环境下{}算法的奖励曲线".format(env, algo), fontproperties=chinese_font())
+    plt.xlabel(u'回合数', fontproperties=chinese_font())
+    ma_std = np.std(ma_rewards, ddof=1)
+    x = np.linspace(0, len(ma_rewards), len(ma_rewards))
+    # for i in range(len(ma_rewards)):
+    #     ma_rewards[i] = ma_rewards[i].reshape(-1)
+    ma_rewards_pstd = [i + ma_std for i in ma_rewards]  # 加方差
+    ma_rewards_pstd = np.array(ma_rewards_pstd)
+    ma_rewards_pstd = ma_rewards_pstd.reshape(1, ma_rewards_pstd.shape[0])[0]
+    ma_rewards_mstd = [i - ma_std for i in ma_rewards]  # 减方差
+    ma_rewards_mstd = np.array(ma_rewards_mstd)
+    ma_rewards_mstd = ma_rewards_mstd.reshape(1, ma_rewards_mstd.shape[0])[0]
+    # plt.plot(rewards)
+    plt.plot(ma_rewards)
+    plt.fill_between(x, ma_rewards_pstd, ma_rewards_mstd, facecolor='blue', alpha=0.3)
+    plt.legend((u'滑动平均奖励', u'奖励',), loc="best", prop=chinese_font())
+    if save:
+        plt.savefig(path + f"{tag}_rewards_curve_cn")
+    # plt.show()
+
+
+def plot_power_cn(power, ma_power, tag="train", env='Optimal Control', algo="DDPG", save=True, path='./'):
+    ''' 中文画图
+    '''
+    sns.set()
+    plt.figure()
+    plt.title(u"{}环境下{}算法的能耗曲线".format(env, algo), fontproperties=chinese_font())
+    plt.xlabel(u'回合数', fontproperties=chinese_font())
+    ma_std = np.std(ma_power, ddof=1)
+    x = np.linspace(0, len(ma_power), len(ma_power))
+    ma_power_pstd = [i + ma_std for i in ma_power]  # 加方差
+    ma_power_pstd = np.array(ma_power_pstd)
+    ma_power_pstd = ma_power_pstd.reshape(1, ma_power_pstd.shape[0])[0]
+    ma_power_mstd = [i - ma_std for i in ma_power]  # 减方差
+    ma_power_mstd = np.array(ma_power_mstd)
+    ma_power_mstd = ma_power_mstd.reshape(1, ma_power_mstd.shape[0])[0]
+    plt.plot(power)
+    plt.plot(ma_power)
+    plt.legend((u'滑动平均能耗', u'能耗',), loc="best", prop=chinese_font())
+    plt.fill_between(x, ma_power_pstd, ma_power_mstd, facecolor='blue', alpha=0.3)
+    if save:
+        plt.savefig(path + f"{tag}_power_curve_cn")
+    # plt.show()
+
+
+def plot_unsafecounts_cn(unsafe_counts, ma_unsafe_counts, tag="train", env='Optimal Control', algo="DDPG", save=True,
+                         path='./'):
+    ''' 中文画图
+    '''
+    sns.set()
+    plt.figure()
+    plt.title(u"{}环境下{}算法的不安全动作次数曲线".format(env, algo), fontproperties=chinese_font())
+    plt.xlabel(u'回合数', fontproperties=chinese_font())
+    # ma_std = np.std(ma_unsafe_counts, ddof=1)
+    # x = np.linspace(0, len(ma_unsafe_counts), len(ma_unsafe_counts))
+    # ma_unsafe_counts_pstd = [i + ma_std for i in ma_unsafe_counts]  # 加方差
+    # ma_unsafe_counts_pstd = np.array(ma_unsafe_counts_pstd)
+    # ma_unsafe_counts_pstd = ma_unsafe_counts_pstd.reshape(1, ma_unsafe_counts_pstd.shape[0])[0]
+    # ma_unsafe_counts_mstd = [i - ma_std for i in ma_unsafe_counts]  # 减方差
+    # ma_unsafe_counts_mstd = np.array(ma_unsafe_counts_mstd)
+    # ma_unsafe_counts_mstd = ma_unsafe_counts_mstd.reshape(1, ma_unsafe_counts_mstd.shape[0])[0]
+    plt.plot(unsafe_counts)
+    plt.plot(ma_unsafe_counts)
+    plt.legend((u'滑动平均不安全次数', u'不安全次数',), loc="best", prop=chinese_font())
+    # plt.fill_between(x, ma_unsafe_counts_pstd, 0, facecolor='blue', alpha=0.3)
+    if save:
+        plt.savefig(path + f"{tag}_unsafe counts_curve_cn")
+    # plt.show()
+
+
+def plot_losses(losses, algo="DQN", save=True, path='./'):
+    sns.set()
+    plt.title("loss curve of {}".format(algo))
+    plt.xlabel('epsiodes')
+    plt.plot(losses, label='rewards')
+    plt.legend()
+    if save:
+        plt.savefig(path + "losses_curve")
+    plt.show()
+
+
+def plot_speed(total_v_list, total_t_list, total_a_list, tag="train", env='Train Optimal', algo="DDPG", save=True,
+               path='./'):
+    sns.set()
+    plt.figure()
+    plt.title(u"{}环境下{}算法的训练速度曲线".format(env, algo), fontproperties=chinese_font())
+    ax1 = plt.axes(projection='3d')
+    for i in range(len(total_v_list)):
+        if i % 50 == 0:
+            a = np.array(total_v_list[i]).reshape(-1)
+            b = np.array(total_t_list[i]).reshape(-1)
+            c = np.linspace(1, len(total_t_list[i]), len(total_t_list[i]))
+            ax1.plot3D(b, c, a)
+    plt.legend((u'速度曲线',), loc="best", prop=chinese_font())
+    if save:
+        plt.savefig(path + f"{tag}_speed_profile_cn")
+    plt.figure()
+    for i in range(len(total_a_list)):
+        if i % 50 == 0:
+            plt.plot(total_a_list[i])
+    plt.legend((u'动作曲线',), loc='best', prop=chinese_font())
+    if save:
+        plt.savefig(path + f"{tag}_action_cn")
+    plt.show()
+
+
+def evalplot_speed(total_v_list, total_t_list, total_a_list, tag="eval", env='Train Optimal', algo="DDPG", save=True,
+                   path='./'):
+    sns.set()
+    plt.figure()
+    plt.title(u"{}环境下{}算法的评价速度曲线".format(env, algo), fontproperties=chinese_font())
+    ax1 = plt.axes(projection='3d')
+    for i in range(len(total_v_list)):
+        if i % 6 == 0:
+            a = np.array(total_v_list[i]).reshape(-1)
+            b = np.array(total_t_list[i]).reshape(-1)
+            c = np.linspace(1, len(total_t_list[i]), len(total_t_list[i]))
+            ax1.plot3D(b, c, a)
+    plt.legend((u'速度曲线',), loc="best", prop=chinese_font())
+    if save:
+        plt.savefig(path + f"{tag}_speed_profile_cn")
+    plt.figure()
+    plt.plot(total_a_list[0])
+    plt.legend((u'动作曲线',), loc='best', prop=chinese_font())
+    if save:
+        plt.savefig(path + f"{tag}_action_cn")
+    plt.show()
+
+
+def plot_trainep_speed(total_v_list, total_t_list, total_a_list, total_ep_list, tag="ep_train", env='Train Optimal',
+                       algo="DDPG", save=True,
+                       path='./'):
+    sns.set()
+    plt.figure()
+    plt.title(u"{}环境下{}算法的训练速度曲线（分幕）".format(env, algo), fontproperties=chinese_font())
+    ax1 = plt.axes(projection='3d')
+    for i in range(len(total_ep_list)):
+        if i % 50 == 0:
+            a = np.array(total_v_list[i]).reshape(-1)
+            b = np.array(total_t_list[i]).reshape(-1)
+            c = np.linspace(total_ep_list[i], total_ep_list[i], len(total_t_list[i]))
+            ax1.plot3D(b, c, a)
+    plt.legend((u'速度曲线',), loc="best", prop=chinese_font())
+    if save:
+        plt.savefig(path + f"{tag}_speed_profile_cn")
+    plt.figure()
+    ax2 = plt.axes(projection='3d')
+    for j in range(len(total_ep_list)):
+        if j % 50 == 0:
+            a1 = np.array(total_a_list[j]).reshape(-1)
+            b1 = np.array(total_t_list[j]).reshape(-1)
+            c1 = np.linspace(total_ep_list[j], total_ep_list[j], len(total_t_list[j]))
+            ax2.plot3D(b1, c1, a1)
+    plt.legend((u'动作曲线',), loc='best', prop=chinese_font())
+    if save:
+        plt.savefig(path + f"{tag}_action_cn")
+    plt.show()
+
+
+def plot_evalep_speed(total_v_list, total_t_list, total_a_list, total_ep_list, tag="ep_eval",
+                      env='Train Optimal',
+                      algo="DDPG", save=True,
+                      path='./'):
+    sns.set()
+    plt.figure()
+    plt.title(u"{}环境下{}算法的评价速度曲线（分幕）".format(env, algo), fontproperties=chinese_font())
+    ax1 = plt.axes(projection='3d')
+    for i in range(len(total_ep_list)):
+        if i % 6 == 0:
+            a = np.array(total_v_list[i]).reshape(-1)
+            b = np.array(total_t_list[i]).reshape(-1)
+            c = np.linspace(total_ep_list[i], total_ep_list[i], len(total_t_list[i]))
+            ax1.plot3D(b, c, a)
+    plt.legend((u'速度曲线',), loc="best", prop=chinese_font())
+    if save:
+        plt.savefig(path + f"{tag}_speed_profile_cn")
+    plt.figure()
+    ax2 = plt.axes(projection='3d')
+    for j in range(len(total_ep_list)):
+        if j % 6 == 0:
+            a1 = np.array(total_a_list[j]).reshape(-1)
+            b1 = np.array(total_t_list[j]).reshape(-1)
+            c1 = np.linspace(total_ep_list[j], total_ep_list[j], len(total_t_list[j]))
+            ax2.plot3D(b1, c1, a1)
+    plt.legend((u'动作曲线',), loc='best', prop=chinese_font())
+    if save:
+        plt.savefig(path + f"{tag}_action_cn")
+    plt.show()
